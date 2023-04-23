fa00efcb8cf89cb6f972eb1e418f99d99d62387b
Hubertius <hubert.kopec@interia.pl>
2023-04-23 23:48:08 +0200

    Init of Login UI

--- MazeBank/src/main/java/com/java/mazebank/controllers/LoginController.java
+++ MazeBank/src/main/java/com/java/mazebank/controllers/LoginController.java
@@ -1,4 +1,16 @@
 package com.java.mazebank.controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public ChoiceBox accountSelector;
+    public Label lblPayeeAddress;
+    public TextField inputPayeeAddress;
+    public Label lblPassword;
+    public TextField inputPassword;
+    public Button btnLogin;
+    public Label lblError;
 }

3e0d3d1dbbcf1edc4f6f50d8506be91d7ba2fa46
Hubertius <hubert.kopec@interia.pl>
2023-04-24 14:46:59 +0200

    Init of client menu UI

---
+++ MazeBank/src/main/java/com/java/mazebank/controllers/client/ClientMenuController.java
@@ -0,0 +1,20 @@
+package com.java.mazebank.controllers.client;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientMenuController implements Initializable {
+    public Button btnDashboard;
+    public Button btnTransaction;
+    public Button btnAccounts;
+    public Button btnProfile;
+    public Button btnLogout;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
